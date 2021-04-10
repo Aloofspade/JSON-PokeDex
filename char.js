8025d37a149e8061f41a8b299fce981cb8afa086
Everett Sweet <sweetem2170@gmail.com>
2021-04-09 17:17:12 -0700

    hi

--- char.js
+++ char.js
@@ -204,87 +204,13 @@ let charizrd = {
     "weight": 905
 }
 
-let charizard = `{
-    "abilities": [
-        {
-            "name": "blaze"
-        },
-        {
-            "name": "solar-power"
-        }
-    ],
-    "base_experience": 240,
-    "height": 17,
-    "id": 6,
-    "is_default": true,
-    "name": "charizard",
-    "order": 7,
-    "stats": [
-        {
-            "base_stat": 78,
-            "effort": 0,
-            "stat": {
-                "name": "hp"
-            }
-        },
-        {
-            "base_stat": 84,
-            "effort": 0,
-            "stat": {
-                "name": "attack"
-            }
-        },
-        {
-            "base_stat": 78,
-            "effort": 0,
-            "stat": {
-                "name": "defense"
-            }
-        },
-        {
-            "base_stat": 109,
-            "effort": 3,
-            "stat": {
-                "name": "special-attack"
-            }
-        },
-        {
-            "base_stat": 85,
-            "effort": 0,
-            "stat": {
-                "name": "special-defense"
-            }
-        },
-        {
-            "base_stat": 100,
-            "effort": 0,
-            "stat": {
-                "name": "speed"
-            }
-        }
-    ],
-    "types": [
-        {
-            "slot": 1,
-            "type": {
-                "name": "fire"
-            }
-        },
-        {
-            "slot": 2,
-            "type": {
-                "name": "flying"
-            }
-        }
-    ],
-    "weight": 905
-}`;
 let chariObj = JSON.parse(charizard);
 let header = document.getElementById("header");
 let section = document.getElementById("section");
 let pokimonName = document.createElement("H1");
 pokimonName.textContent = `${chariObj["name"]}`;
 header.appendChild(pokimonName);
-function chariHeader(){
-    
+
+function chariHeader() {
+
 }
