da82fabff62cc3d48dfa1d1d6684b9ac0a2889d9
Aloofspade <abditorres71@gmail.com>
2021-04-22 20:09:35 -0700

    charmeleon

--- char.js
+++ char.js
@@ -393,7 +393,8 @@ function createHeader(obj){
     //base 
     const h2 = document.createElement("h2");
     h2.textContent = `Base Experience:`;
-    header2.appendChild(h2)
+    header2.appendChild(h2);
+
     const p1 = document.createElement("p1");
     p1.textContent = ` ${obj["base_experience"]}`
     header2.appendChild(p1)
